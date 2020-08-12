30b4071dbc12ac36bf6c7c3be97d4a0309515dd3
Adamj291 <66163717+Adamj291@users.noreply.github.com>
2020-08-12 22:46:10 +0100

    Delete 2-efi.sh

--- 2-efi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -d "/sys/firmware/efi/efivars" ]
-    then echo "EFI Partition Exists, Please partition and mount drives and then run 3-pacstrap.sh"
-else echo "Please restart the installation in UEFI"
-fi
