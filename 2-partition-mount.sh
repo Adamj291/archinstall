23ab398bc0f34c5385c01eedfaa0287496c3f06f
Adamj291 <66163717+Adamj291@users.noreply.github.com>
2020-08-13 13:30:38 +0100

    Add files via upload

--- 2-partition-mount.sh
+++ 2-partition-mount.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cfdisk && lsblk -n | grep 'sda\|sdb\|nvm' &&
-read -p 'whats is the swap partiton? /dev/?' swapvar &&
-read -p 'whats is the efi partiton? /dev/?' efivar &&
-read -p 'whats is the rooit partiton? /dev/?' rootvar &&
-mkfs.fat -F3 /dev/$efivar &&
+read -p 'whats is the swap partiton? /dev/? ' swapvar &&
+read -p 'whats is the efi partiton? /dev/? ' efivar &&
+read -p 'whats is the root partiton? /dev/? ' rootvar &&
+mkfs.fat -F32 /dev/$efivar &&
 mkswap /dev/$swapvar &&
 swapon /dev/$swapvar &&
 mkfs.ext4 /dev/$rootvar &&
