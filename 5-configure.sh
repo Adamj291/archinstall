7e9dfd80d88d9a5777a5d857f56ef9ba774d336c
Adamj291 <66163717+Adamj291@users.noreply.github.com>
2020-08-13 18:40:24 +0100

    Add files via upload

--- 5-configure.sh
+++ 5-configure.sh
@@ -15,13 +15,13 @@ mkinitcpio -P &&
 sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" /etc/sudoers && echo "sudoers file updated for wheel account" &&
 # echo "Please change root password" &&
 # passwd root &&
-read -p "Please change root account password " rootpw &&
+read -p 'Please change root account password ' rootpw &&
 echo root:$rootpw | chpasswd &&
 read -p 'whats your username? ' uservar &&
 useradd -m -G wheel -s /bin/bash $uservar &&
 #echo "Please change the $uservar user account password" &&
 #passwd $uservar &&
-read -p "Please change $uservar account password " userpw &&
+read -p 'Please change '$uservar' account password ' userpw &&
 echo $uservar:$userpw | chpasswd &&
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=Arch &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
