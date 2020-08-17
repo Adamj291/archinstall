20f4b29f40e53cb56456bb17045b2cb3dff7a90e
Adamj291 <66163717+Adamj291@users.noreply.github.com>
2020-08-17 20:58:36 +0100

    Add files via upload

--- 4-configure.sh
+++ 4-configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 genfstab -U /mnt >> /mnt/etc/fstab && echo "fstab has been generated" &&
-cp ./5-configure.sh /mnt/tmp &&
-arch-chroot /mnt /mnt/tmp/5-configure.sh
+cp ./5-configure.sh /mnt && echo "Please run ./5-configure.sh" &&
+arch-chroot /mnt
